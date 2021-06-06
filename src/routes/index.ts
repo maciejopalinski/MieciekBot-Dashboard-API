dfe1fb7b66eb6f77f28658a70fc6a2155028fecd
Maciej Opaliński <poprostumieciek@gmail.com>
2021-06-06 10:39:19 +0200

    Add version info to /api endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,15 @@ import { Router } from 'express';
 import auth from './auth';
 import discord from './discord';
 
+import package_info from '../../package.json';
+const version = package_info.version + (process.env.DEBUG == 'true' ? '-dev' : '');
+
 const router = Router();
 
 router.get('/', (req, res) => {
     res.send({
-        message: 'MieciekBot Dashboard API'
+        message: 'MieciekBot Dashboard API',
+        version,
     });
 });
 
